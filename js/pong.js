09bf5d320ec9f8986b78a3742af3708877382293
Daniel Bispo <daniel.bispo@bemobi.com.br>
2018-10-08 15:23:03 -0300

    starting project by adding an index html and starting the main js file with the animate function

---
+++ js/pong.js
@@ -0,0 +1,4 @@
+var animate = window.requestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+function(callback) { window.setTimeout(callback, 1000/60) };
