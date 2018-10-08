5089c14b1d3ac1877df4c55c4b4afcc83bd283ec
Daniel Bispo <daniel.bispo@bemobi.com.br>
2018-10-08 16:56:00 -0300

    adding ball, computer logic and collision

--- js/pong.js
+++ js/pong.js
@@ -13,6 +13,8 @@ var player = new Player();
 var computer = new Computer();
 var ball = new Ball(200, 300);
 
+var keysDown = {};
+
 window.onload = () => {
     document.body.appendChild(canvas);
     animate(step);
@@ -25,7 +27,9 @@ var step = () => {
 };
 
 var update = () => { 
-
+    player.update();
+    computer.update(ball);
+    ball.update(player.paddle, computer.paddle);
 };
 
 var render = () => {
@@ -50,20 +54,63 @@ Paddle.prototype.render = function() {
     context.fillRect(this.x, this.y, this.width, this.height);
 };
 
-function Player() {
-    this.paddle = new Paddle(175, 580, 50, 10);
+Paddle.prototype.move = function(x,y) {
+    this.x += x;
+    this.y += y;
+    this.x_speed = x;
+    this.y_speed = y;
+    if (this.x < 0) { // all to left
+        this.x = 0;
+        this.x_speed = 0;
+    } else if (this.x + this.width > 400) { //all to right
+        this.x = 400 - this.width;
+        this.x_speed = 0;
+    }
 };
 
-function Computer() {
-    this.paddle = new Paddle(175, 10, 50, 10);
+function Player() {
+    this.paddle = new Paddle(175, 580, 50, 10);
 };
 
 Player.prototype.render = function() {
     this.paddle.render();
 };
 
+Player.prototype.update = function() {
+    for (var key in keysDown) {
+        var value = Number(key);
+        if (value == 37) { //left arrow
+            this.paddle.move(-4, 0);
+        } else if (value == 39) { //right arrow
+            this.paddle.move(4, 0);
+        } else {
+            this.paddle.move(0,0);
+        }
+    }
+};
+
+function Computer() {
+    this.paddle = new Paddle(175, 10, 50, 10);
+};
+
 Computer.prototype.render = function() {
     this.paddle.render();
+};
+
+Computer.prototype.update = function(ball) {
+    var x_pos = ball.x;
+    var diff = -((this.paddle.x + (this.paddle.width /2)) - x_pos);
+    if (diff < 0 && diff < -4) { //max speed left
+        diff = -5;
+    } else if(diff > 0 && diff > 4) { //max speed right
+        diff = 5;
+    }
+    this.paddle.move(diff, 0);
+    if (this.paddle.x < 0) {
+        this.paddle.x = 0;
+    } else if ((this.paddle.x + this.paddle.width) > 400) {
+        this.paddle.x = 400 - this.paddle.width;
+    }
 };
 
 function Ball(x,y) {
@@ -80,3 +127,55 @@ Ball.prototype.render = function() {
     context.fillStyle - "#FFF";
     context.fill();
 };
+
+Ball.prototype.update = function(paddle1, paddle2) {
+    this.x += this.x_speed;
+    this.y += this.y_speed;
+    var top_x = this.x - 5;
+    var top_y = this.y - 5;
+    var bottom_x = this.x + 5;
+    var bottom_y = this.y + 5;
+
+    if(this.x - 5 < 0) { //hits the left wall 
+        this.x = 5;
+        this.x_speed = -this.x_speed;
+    } else if (this.x + 5 > 400) { //hits the left wall
+        this.x = 395;
+        this.x_speed = -this.x_speed;
+    }
+
+    if (this.y < 0 || this.y > 600) { //scoring points
+        this.x_speed = 0;
+        this.y_speed = 3;
+        this.x = 200;
+        this.y = 300;
+    }
+
+    if (top_y > 300) {
+        if (top_y < (paddle1.y + paddle1.height) && 
+            bottom_y > paddle1.y &&
+            top_x < (paddle1.x + paddle1.width) &&
+            bottom_x > paddle1.x) { //hitting player's paddle
+            this.y_speed = -3;
+            this.x_speed += (paddle1.x_speed /2);
+            this.y += this.y_speed;
+        }
+    } else {
+        if (top_y < (paddle2.y + paddle2.height) && 
+            bottom_y > paddle2.y && 
+            top_x < (paddle2.x + paddle2.width) && 
+            bottom_x > paddle2.x) { //hitting computer's paddle
+            this.y_speed = 3;
+            this.x_speed += (paddle2.x_speed /2);
+            this.y += this.y_speed;
+        }
+    }
+};
+
+window.addEventListener("keydown", function(event) {
+    keysDown[event.keyCode] = true;
+});
+
+window.addEventListener("keyup", function(event) {
+    delete keysDown[event.keyCode];
+});
