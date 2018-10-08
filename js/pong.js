870194dc75620003807f04daebcc07a7fcf2b809
Daniel Bispo <daniel.bispo@bemobi.com.br>
2018-10-08 15:33:18 -0300

    creating a canvas and the necessary functions for it to render

--- js/pong.js
+++ js/pong.js
@@ -2,3 +2,30 @@ var animate = window.requestAnimationFrame ||
 window.webkitRequestAnimationFrame ||
 window.mozRequestAnimationFrame ||
 function(callback) { window.setTimeout(callback, 1000/60) };
+
+var canvas = document.createElement('canvas');
+var width = 400;
+var height = 600;
+canvas.width = width;
+canvas.height = height;
+var context = canvas.getContext('2d');
+
+window.onload = () => {
+    document.body.appendChild(canvas);
+    animate(step);
+};
+
+var step = () => {
+    update();
+    render();
+    animate(step);
+};
+
+var update = () => { 
+
+};
+
+var render = () => {
+    context.fillStyle = "#000000";
+    context.fillRect(0, 0, width, height);
+};
